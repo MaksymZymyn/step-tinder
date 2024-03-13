cde5d1e2a57815f95de2425aacd90d56f9481aa7
Luka Ponomarenko <luka.ponomarenko@gmail.com>
2024-03-13 23:15:38 +0200

    docker removed. added remote development database on AWS.

--- src/main/java/utils/environment/ConnectionDetails.java
+++ src/main/java/utils/environment/ConnectionDetails.java
@@ -2,7 +2,7 @@ package utils.environment;
 
 class ConnectionDetails {
 
-    public static final String url = "jdbc:postgresql://localhost:9090/tinder";
+    public static final String url = "jdbc:postgresql://step-tinder.c5g0cgqqgsrf.eu-north-1.rds.amazonaws.com:5432/postgres";
     public static final String username = "postgres";
     public static final String password = "pg123456";
 
