f07ea1bb485dd50d0a759b05246bd560a807843a
admin <maksym.rizdvjanyj@gmail.com>
2024-03-14 14:10:20 +0200

    add changes in ConnectionDetails

--- src/main/java/utils/environment/ConnectionDetails.java
+++ src/main/java/utils/environment/ConnectionDetails.java
@@ -1,32 +1,16 @@
 package utils.environment;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
 public class ConnectionDetails {
 
-    private static final String PROPERTIES_FILE = "database.properties";
-    private static final Properties properties = new Properties();
-
-    static {
-        try (InputStream input = new FileInputStream(PROPERTIES_FILE)) {
-            properties.load(input);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-
     public static String getUrl() {
-        return properties.getProperty("url");
+        return System.getenv("jdbc_url");
     }
 
     public static String getUsername() {
-        return properties.getProperty("username");
+        return System.getenv("jdbc_username");
     }
 
     public static String getPassword() {
-        return properties.getProperty("password");
+        return System.getenv("jdbc_password");
     }
 }
