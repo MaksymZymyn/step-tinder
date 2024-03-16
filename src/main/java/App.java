3045566621fd6e41c8a5a04698cad51eb080b305
Luka Ponomarenko <luka.ponomarenko@gmail.com>
2024-03-16 13:56:27 +0200

    removed unnecessary lines from App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,6 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import servlets.StaticFileServlet;
-import users.UserDAO;
 import utils.environment.HerokuEnv;
 
 public class App {
@@ -12,9 +11,6 @@ public class App {
         ServletContextHandler handler = new ServletContextHandler();
 
         handler.addServlet(new ServletHolder(new StaticFileServlet("static")), "/static/*");
-
-        UserDAO dao = new UserDAO();
-        dao.insert("yalukaiwo", "Ponomarenko Luka", "asd", "password");
 
         server.setHandler(handler);
 
